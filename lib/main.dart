b038c6e6b38b462004e97f6d448f371f5f8b3e29
achreflajmi <achref.lajmi@esprit.tn>
2025-02-17 21:53:21 +0100

    clean architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'ui/signup_page.dart'; // Import signup page
 import 'ui/login_page.dart'; // Import login page
+import 'view_models/authentication_view_model.dart';
+import 'view_models/chatbot_viewmodel.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +12,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chatbot App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthenticationViewModel()),
+        ChangeNotifierProvider(create: (_) => ChatbotViewModel()),
+      ],
+      child: MaterialApp(
+        title: 'Chatbot App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: SignupPage(), // Start with SignupPage
       ),
-      home: SignupPage(), // Start with SignupPage
     );
   }
 }
